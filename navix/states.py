a10786b70e865742e6988b243b75f9baecc80268
Ruo Yu Tao <ruoyutao@gmail.com>
2025-02-05 10:45:06 -0500

    default fields

--- navix/states.py
+++ navix/states.py
@@ -63,10 +63,10 @@ class Event(Positionable, HasColour):
         happened (Array): A boolean flag indicating whether the event happened.
         event_type (Array): The type of event that happened."""
 
-    position: Array = jnp.asarray([-1, -1], dtype=jnp.int32)
-    colour: Array = PALETTE.UNSET
-    happened: Array = jnp.asarray(False, dtype=jnp.bool_)
-    event_type: Array = EventType.NONE
+    position: Array = field(default_factory=lambda: jnp.asarray([-1, -1], dtype=jnp.int32))
+    colour: Array = field(default_factory=lambda: PALETTE.UNSET)
+    happened: Array = field(default_factory=lambda: jnp.asarray(False, dtype=jnp.bool_))
+    event_type: Array = field(default_factory=lambda: EventType.NONE)
 
     def __eq__(self, other: Event) -> Array:
         return jnp.logical_and(
